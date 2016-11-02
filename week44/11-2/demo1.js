3af1945f0c781f5d7690410faa63b48a922a0f28
fanbrightup <fanbright@126.com>
2016-11-02 08:17:01 +0800

    Auto-commit

---
+++ week44/11-2/demo1.js
@@ -0,0 +1,26 @@
+// console.log('hh');
+require('shelljs/global');
+if (!which('git')) {
+  echo('Sorry, this script requires git');
+  exit(1);
+}
+ 
+// Copy files to release dir 
+rm('-rf', 'out/Release');
+cp('-R', 'stuff/', 'out/Release');
+ 
+// Replace macros in each .js file 
+cd('lib');
+ls('*.js').forEach(function(file) {
+  sed('-i', 'v0.1.2', 'v0.1.2', file);
+
+  sed('-i', /.*REPLACE_LINE_WITH_MACRO.*\n/, cat('macro.js'), file);
+});
+cd('..');
+exec('git add .');
+ 
+// Run external tool synchronously 
+ if (exec('git commit -am "Auto-commit"').code !== 0) {
+ echo('Error: Git commit failed');
+ exit(1);
+}
